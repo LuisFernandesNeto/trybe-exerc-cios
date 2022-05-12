f15c16d80d0a266345c23097393d38e8d537b779
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-12 16:22:09 -0300

    exercicio JS 1

---
+++ fundamentos/bloco-4-introducao-java-script/script.js
@@ -0,0 +1,15 @@
+const myName = "Luis";
+
+const birthCity = "Itaquaquecetuba";
+
+let birthYear = 1991;
+
+console.log(myName,birthCity,birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "São Paulo";
+
+console.log(birthCity);
