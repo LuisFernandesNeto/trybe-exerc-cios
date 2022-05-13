9c20ddb2040f582418ddbb4b014fdfe9c89b0c57
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-13 15:36:39 -0300

    Exercício for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -19,3 +19,11 @@ let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu3.push('Contato');
 
 console.log(menu3);
+
+// Exercício 4
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let indice = 0; indice < groceryList.length; indice += 1) {
+    console.log(groceryList[indice])
+}
