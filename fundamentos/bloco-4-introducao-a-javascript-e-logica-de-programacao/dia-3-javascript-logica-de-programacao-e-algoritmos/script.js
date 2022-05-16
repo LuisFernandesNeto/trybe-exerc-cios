fec5f4a8871c67d3c0e713516f1ee8ea4c9f951f
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-16 19:19:43 -0300

    Dia 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,54 @@
+// Exercício 1
+
+let fatorial = 10;
+
+let resultado = fatorial;
+
+for (let n = 1; n < fatorial; n += 1) {
+    resultado *= n;
+}
+
+console.log(resultado);
+
+
+// Exercício 2
+
+let word = 'tryber';
+
+let newWord = "";
+
+for (let i = word.length - 1; i >= 0; i -= 1) {
+    newWord += word[i];
+}   
+
+console.log(newWord);
+
+// Exercício 3
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = '';
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > maior.length) {
+        maior = array[i];
+    }
+}
+
+console.log(maior);
+
+
+// Exercício 4
+
+let numero = 50;
+let primo;
+
+for (n = 2; n <= numero; n += 1) {
+    for (let divisor = 2; divisor < n; divisor += 1) {
+        if (n % divisor === 0)
+        break; 
+    }
+    primo = n;
+}   
+
+console.log(primo);
