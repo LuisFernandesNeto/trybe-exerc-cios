5903cf2d270ea2e866669b1afc83189255d8c5cc
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-19 18:28:50 -0300

    Alguns arquivos e exercicio 1 do conteúdo 5.1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/example2.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByClassName('paragraph'));
+
+console.log(document.getElementsByClassName('paragraph')[0].innerText = 'Filme preferido');
+
+
+
+console.log(document.getElementById("subtitle").style.color = "red");
