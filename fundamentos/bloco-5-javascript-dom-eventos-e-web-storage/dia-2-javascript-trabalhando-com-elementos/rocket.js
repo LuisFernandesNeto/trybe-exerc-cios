de50782e10ac56d476ef996a82bcd442ec26af7f
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-20 15:56:17 -0300

    Examplo 1 e exercício rocket 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/rocket.js
@@ -0,0 +1,15 @@
+console.log(document.getElementById("elementoOndeVoceEsta"));
+
+console.log(document.getElementById("elementoOndeVoceEsta").parentElement.style.color = "red");
+
+console.log(document.getElementById("primeiroFilhoDoFilho").innerText = " Zooboomafu");
+
+console.log(document.getElementById("pai").firstElementChild);
+
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").firstChild);
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
+
+console.log(document.getElementById("pai").nextElementSibling);
