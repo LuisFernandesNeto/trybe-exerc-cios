6552ec751d146e337fa985abb26e207df909f4c1
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-06-01 13:52:05 -0300

    Bloco 6.1, Exercício

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script3.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script3.js
@@ -1,5 +1,18 @@
-let button = document.getElementById("button");
+/* let button = document.getElementById("button");
 
 button.addEventListener("click", function(event) {
     event.preventDefault()
-})
+}) */
+
+let nome = document.getElementById("nome").innerText;
+let email = document.getElementById("email").innerText;
+let button = document.getElementById("button");
+
+    button.addEventListener("click", function() {
+        if (nome.length > 40 && email.length < 10 && email.length > 50) {
+            alert("Dados Inválidos");
+        } else {
+            alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.");
+        }
+    })
+    
