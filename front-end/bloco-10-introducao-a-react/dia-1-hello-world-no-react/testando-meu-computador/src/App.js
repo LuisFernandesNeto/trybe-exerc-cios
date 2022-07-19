e3ed4068da32a8a3f4df4eb53ace0e1b28674793
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-07-19 14:00:12 -0300

    Bloco 10

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const testJSX = 'Hello, JSX';
+
+  return (
+    <div className="App">
+     <h1> { testJSX } </h1>
+    </div>
+  );
+}
+
+export default App;
+
