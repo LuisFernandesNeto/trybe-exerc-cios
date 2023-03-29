13643a14a13ebbf17d7185c8fcacf445358d35fd
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2023-03-29 16:45:27 -0300

    bloco 2 dia 1

---
+++ cs/bloco-2-poo-em-python/dia-1/exercise-1.py
@@ -0,0 +1,53 @@
+class Ventilador:
+    def __init__(self, cor, potencia, tensao, preco) -> None:
+        self.cor = cor
+        self._potencia = potencia
+        self._tensao = tensao
+        self.preco = preco
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.ventilador = None
+
+    def comprar_ventilador(self, ventilador):
+        if ventilador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= ventilador.preco
+            self.ventilador = ventilador
+
+    def __str__(self):
+        if (self.ventilador):
+            return f"{self.nome} - possui um ventilador."
+        return f"{self.nome} - não possui um ventilador."
+
+
+ventilador_branco = Ventilador("branco", potencia=250, tensao=220, preco=100)
+pessoa = Pessoa("Maria", saldo_na_conta=2000)
+pessoa.comprar_ventilador(ventilador_branco)
+
+print(pessoa)
